03730e72d7ffd7584a504f0bd3f2ac8c5fdf6a47
Yuriy_Jurayev <yuriy_jurayev@epam.com>
2018-01-16 17:27:06 +0600

    added logging

--- src/main/java/kz/epam/atm/gmailtestPF/driver/FactoryDriver.java
+++ src/main/java/kz/epam/atm/gmailtestPF/driver/FactoryDriver.java
@@ -1,6 +1,8 @@
 package kz.epam.atm.gmailtestPF.driver;
 
 import kz.epam.atm.gmailtestPF.property.PropertyProvider;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,6 +14,7 @@ public class FactoryDriver {
 
     private static WebDriver driver;
     private static String browserName;
+    private static Logger log = LogManager.getRootLogger();
 
     public static void setBrowserName(String browserName) {
         FactoryDriver.browserName = browserName;
@@ -23,18 +26,23 @@ public class FactoryDriver {
         return driver;
     }
     public static void closeDriver() {
+        log.info("try to close driver");
         driver.quit();
         driver = null;
+        log.info("driver is closed");
     }
 
     private static void getCurrentDriver(){
         BrowserTypes browser = BrowserTypes.valueOf(browserName);
+        log.info("try to create driver");
         switch (browser){
             case CHROME:
                 driver = new CustomWebDriver(createChromeDriver());
+                log.info("chrome driver is created");
                 break;
             default:
                 driver = new CustomWebDriver(createFirefoxDriver());
+                log.info("firefox driver is created");
                 break;
         }
         driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
